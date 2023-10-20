10e9fbf074d695140b29c32d73f0c943b287c1e0
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-10-19 21:32:44 -0400

    basic homepage complete

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask, render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from stitch import stitch, hello_flask
+import cv2 as cv
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/uploads'
+app.config['UPLOAD_FOLDER'] = os.path.abspath('static/uploads')
+app.config['RESULT_FOLDER'] = os.path.abspath('static/results')  # Set the absolute path for the results
 
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -15,8 +19,8 @@ def allowed_file(filename):
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == 'GET':
-        print("Whats good")
-
+        # print("Whats good")
+        print(hello_flask())
         return render_template('index.html')
     if request.method == 'POST':
         print("RECIEVED POST REQUEST")
@@ -35,19 +39,31 @@ def home():
         if left_image and allowed_file(left_image.filename) and right_image and allowed_file(right_image.filename):
             left_image_filename = secure_filename(left_image.filename)
             left_image.save(os.path.join(app.config['UPLOAD_FOLDER'], left_image_filename))
-
             right_image_filename = secure_filename(right_image.filename)
             right_image.save(os.path.join(app.config['UPLOAD_FOLDER'], right_image_filename))
+
+            # totally could just use a stream, but for now this feels easier
+
+            img1 = cv.imread(os.path.join(app.config['UPLOAD_FOLDER'], left_image_filename), 1)
+            img2 = cv.imread(os.path.join(app.config['UPLOAD_FOLDER'], right_image_filename), 1)
             
-                        
-            return redirect(request.url)
+            result_img = stitch(img2, img1)
 
-        return render_template('index.html', result="result")
+            h, w, c = result_img.shape
+            result_img_filename =  os.path.join(app.config['RESULT_FOLDER'],'result.png')
+            cv.imwrite(result_img_filename, result_img)
+            print(result_img_filename)
+            return  render_template('result.html', result_image='static/results/result.png', height=h, width=w) # render_template('result.html', result_image="/uploads" + left_image_filename)
+        return render_template('index.html')
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    print("ayo we made it")
-    if request.method == 'POST':
-        print(request.files)
-        # file.save(f"/var/www/uploads/{secure_filename(file.filename)}")
+
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template('about.html')
+
+
+@app.route("/demo")
+def demo():
+    return render_template('demo.html')
