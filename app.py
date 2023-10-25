8df3861a0aa6242e53e2c9a3589110dddcc04085
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-10-25 14:03:25 -0400

    added logging for vercel

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from werkzeug.utils import secure_filename
 import datetime
 from aws_utilities import lambda_get_request, upload_file, download_file
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = os.path.abspath("static/uploads")
 app.config["RESULT_FOLDER"] = os.path.abspath("static/results")
@@ -21,66 +26,65 @@ def allowed_file(filename):
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "GET":
-        # print("Whats good")
+        # logging.debug("Whats good")
+        logging.debug("GET /")
+
         return render_template("index.html")
     if request.method == "POST":
-        print("RECIEVED POST REQUEST")
-        print(request.files)
+        logging.debug("POST /")
+
+        logging.debug(request.files)
         if "left_image" not in request.files or "right_image" not in request.files:
-            print("Missing files")
+            logging.debug("Missing files")
             return redirect(request.url)
 
         left_image = request.files["left_image"]
         right_image = request.files["right_image"]
 
         if left_image.filename == "" or right_image.filename == "":
-            print("no images uploaded")
+            logging.debug("no images uploaded")
             return redirect(request.url)
 
-        if (
-            left_image
-            and allowed_file(left_image.filename)
-            and right_image
-            and allowed_file(right_image.filename)
-        ):
+        if (left_image and allowed_file(left_image.filename) and right_image and allowed_file(right_image.filename)):
             left_image_filename = secure_filename(left_image.filename)
-            left_image_path = os.path.join(
-                app.config["UPLOAD_FOLDER"], left_image_filename
-            )
+            left_image_path = os.path.join(app.config["UPLOAD_FOLDER"], left_image_filename)
             left_image.save(left_image_path)
 
             right_image_filename = secure_filename(right_image.filename)
-            right_image_path = os.path.join(
-                app.config["UPLOAD_FOLDER"], right_image_filename
-            )
+            right_image_path = os.path.join(app.config["UPLOAD_FOLDER"], right_image_filename)
             right_image.save(right_image_path)
 
             # upload left and right file to s3 bucket
+            logging.debug("Uploading to s3")
             res_left = upload_file(left_image_path, BUCKET)
             res_right = upload_file(right_image_path, BUCKET)
 
             if not res_left or not res_right:
-                print("error uploading files to s3")
+                logging.debug("error uploading files to s3")
                 return
+
+            logging.debug("Finished s3 upload")
 
             # trigger lambda and get name of the result file
 
-            result_filename = lambda_get_request(
-                LAMBDA_URL, left_image_filename, right_image_filename
-            )
+            logging.debug("Starting Lambda")
 
-            print(result_filename)
+            result_filename = lambda_get_request(LAMBDA_URL, left_image_filename, right_image_filename)
+
             if result_filename == "":
-                print("error during lambda function execution")
+                logging.debug("error during lambda function execution")
                 return
+            
+            logging.debug(f"Finished Lambda. result_filename: {result_filename}")
+
 
             # get result file from s3
             # result_image_fullpath = os.path.join(app.config['RESULT_FOLDER'], result_filename)
-            # print(result_image_fullpath)
+            # logging.debug(result_image_fullpath)
             # res_download = download_file(result_filename, BUCKET, result_image_fullpath)
 
             image_url = BUCKET_URL + result_filename
-            print(image_url)
+            logging.debug(image_url)
             return render_template("result.html", image_url=image_url)
         return render_template("index.html")
 
