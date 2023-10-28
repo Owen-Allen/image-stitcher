4926d40dda270a841bb502fd0d86f677b5e931be
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-10-28 17:09:05 -0400

    changed upload and download logic to client side, rather than server side.

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 import os
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 import datetime
-from aws_utilities import lambda_get_request, upload_file, download_file
-
+from aws_utilities import lambda_get_request, upload_file, download_file, create_presigned_post
 import logging
 
-logging.basicConfig(level=logging.INFO)
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = os.path.abspath("static/uploads")
 app.config["RESULT_FOLDER"] = os.path.abspath("static/results")
+logging.basicConfig(level=logging.INFO)
 
-LAMBDA_URL = "https://jp8nmjnyo0.execute-api.us-west-2.amazonaws.com/create-stitch/image-stitcher"
-BUCKET_URL = "https://opencv-data.s3.us-east-2.amazonaws.com/"
-BUCKET = "opencv-data"
+LAMBDA_URL = os.environ.get('LAMBDA_URL')
+BUCKET_URL = os.environ.get('BUCKET_URL')
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
+
+
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
-
-
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -29,64 +29,38 @@ def home():
         logging.info("GET /")
 
         return render_template("index.html")
+
+@app.route('/result', methods=["POST"])
+def result():
+    print('IN RESULT')
     if request.method == "POST":
-        logging.info("POST /")
+        print('here')
+        if 'left' not in request.json or 'right' not in request.json:
+            return 'Missing Parameters', 400
+        
+        print("here2")
+        
+        left_filename = request.json['left']
+        right_filename = request.json['right']
+        print("here 3")
 
-        logging.info(request.files)
-        if "left_image" not in request.files or "right_image" not in request.files:
-            logging.info("Missing files")
-            return redirect(request.url)
+        result_filename = lambda_get_request(LAMBDA_URL, left_filename, right_filename)
 
-        left_image = request.files["left_image"]
-        right_image = request.files["right_image"]
+        if result_filename == "":
+            app.logger.info("error during lambda function execution")
+            return
+        
+        app.logger.info(f"Finished Lambda. result_filename: {result_filename}")
 
-        if left_image.filename == "" or right_image.filename == "":
-            logging.info("no images uploaded")
-            return redirect(request.url)
-
-        if (left_image and allowed_file(left_image.filename) and right_image and allowed_file(right_image.filename)):
-            left_image_filename = secure_filename(left_image.filename)
-            left_image_path = os.path.join(app.config["UPLOAD_FOLDER"], left_image_filename)
-            left_image.save(left_image_path)
-
-            right_image_filename = secure_filename(right_image.filename)
-            right_image_path = os.path.join(app.config["UPLOAD_FOLDER"], right_image_filename)
-            right_image.save(right_image_path)
-
-            # upload left and right file to s3 bucket
-            logging.info("Uploading to s3")
-            res_left = upload_file(left_image_path, BUCKET)
-            res_right = upload_file(right_image_path, BUCKET)
-
-            if not res_left or not res_right:
-                logging.info("error uploading files to s3")
-                return
-
-            logging.info("Finished s3 upload")
-
-            # trigger lambda and get name of the result file
-
-            logging.info("Starting Lambda")
-
-            result_filename = lambda_get_request(LAMBDA_URL, left_image_filename, right_image_filename)
-
-            if result_filename == "":
-                logging.info("error during lambda function execution")
-                return
-            
-            logging.info(f"Finished Lambda. result_filename: {result_filename}")
+        image_url = BUCKET_URL + result_filename
+        data = {'image_url': image_url}
+        return data
 
 
-            # get result file from s3
-            # result_image_fullpath = os.path.join(app.config['RESULT_FOLDER'], result_filename)
-            # logging.info(result_image_fullpath)
-            # res_download = download_file(result_filename, BUCKET, result_image_fullpath)
-
-            image_url = BUCKET_URL + result_filename
-            logging.info(image_url)
-            return render_template("result.html", image_url=image_url)
-        return render_template("index.html")
-
+        # get result file from s3
+        # result_image_fullpath = os.path.join(app.config['RESULT_FOLDER'], result_filename)
+        # logging.info(result_image_fullpath)
+        # res_download = download_file(result_filename, BUCKET_NAME, result_image_fullpath)
 
 @app.route("/about", methods=["GET", "POST"])
 def about():
@@ -96,3 +70,25 @@ def about():
 @app.route("/demo")
 def demo():
     return render_template("demo.html")
+
+import requests
+@app.route('/upload_url', methods=["POST"])
+def upload_url():
+    if request.method == "POST":
+        req_data = request.get_json()
+        file_name = req_data['file_name']
+        # file_type = req_data['type']
+        file_type = 'image/png'
+        response = create_presigned_post(BUCKET_NAME, file_name)
+
+        # with open('static/left_art_small.png', 'rb') as f:
+        #     files = {'file': (file_name, f)}
+        #     print(files)
+        #     http_response = requests.post(response['url'], data=response['fields'], files=files)
+
+        # logging.info(f'File upload HTTP status code: {http_response.status_code}')
+
+        return response
+
+    return  418, "its tea time"
+    
--- static/js/index.js
+++ static/js/index.js
@@ -1,43 +1,74 @@
 
 
 
-function allowDrop(event) {
-    event.preventDefault();
+async function uploadFile(file){
+    let backendResponse = await fetch(`/upload_url`, {
+        method : 'POST',
+        headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({file_name: file.name, type: file.type})
+    })
+
+    if (backendResponse.status != 200){
+        alert('error during url request to backend')
+        return false
+    }
+
+    data = await backendResponse.json()
+
+    const formData = new FormData();
+    formData.append('key', file.name);
+    formData.append('Content-Type', file.type);
+    formData.append('file', file);
+  
+    let s3Response = await fetch(data.url, {
+        method:"POST", 
+        body: formData
+    })
+
+    if (!s3Response.ok){
+        console.log('Error uploading file to s3')
+        console.log(s3Response)
+        return false
+    }
+    return true
 }
 
-function drag(event) {
-    console.log('DRAG')
-    console.log(event)
-    event.dataTransfer.setData("text", event.target.id);
-}
 
+async function submitForm(){
+    const leftFile = document.getElementById("left_image").files[0]
+    const rightFile = document.getElementById("right_image").files[0]
 
+    if(!leftFile || !rightFile){
+        alert('Please select 2 files to stitch')
+        return
+    }
 
-function dropLeft(event) {
-    console.log('DROP LEFT')
+    if(!uploadFile(leftFile) || !uploadFile(rightFile)){
+        alert('error uploading files to s3')
+        return
+    }
 
-    console.log(event.dataTransfer.files);
+    const data = { left: leftFile.name, right: rightFile.name };
 
-    // event.preventDefault();
-    // var data = event.dataTransfer.getData("text");
-    // console.log(event)
-    // console.log(event.dataUrl)
+    const resultResponse = await fetch('/result', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    console.log(resultResponse)
+    
+    console.log('here 1')
+    let resData = await resultResponse.json()
+    console.log('resData')
+    console.log(resData)
 
-    // var img = document.getElementById(data);
-    // var canvas = document.createElement('canvas');
-    // var ctx = canvas.getContext('2d');
-    // ctx.drawImage(img, 0, 0);
-    // var dataURL = canvas.toDataURL();
-    // document.getElementById('left_image_input').value = dataURL;
-}
+    let resultUrl = resData.image_url;
 
-function dropRight(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    var img = document.getElementById(data);
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-    var dataURL = canvas.toDataURL();
-    document.getElementById('right_image_input').value = dataURL;
+    let image = document.getElementById("result");
+    image.src = resultUrl; // Set the image source to the filename
+    console.log(resultUrl)
+    image.style.display = "block";
+    console.log("DONE")
 }
